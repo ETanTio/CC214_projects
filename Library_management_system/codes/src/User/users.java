b545b326fe78b395fec8482c8cc0bae5694c2476
ETanTio <166849358+ETanTio@users.noreply.github.com>
2024-12-28 04:32:34 +0800

    Update users.java

--- Library_management_system/codes/src/User/users.java
+++ Library_management_system/codes/src/User/users.java
@@ -1,19 +1,28 @@
 package User;
 
+import java.util.*;
+import SettersAndGetters.Books;
+
 public class users {
     private String Name;
     private int Id;
     private String email;
     private String pass;
+    private List<Books> borrowedBooks;
+    private List<Books> returnedBooks;
 
-
-    public users(int Id, String Name, String pass, String email){
+    public users(int Id, String Name, String pass, String email) {
         this.Id = Id;
         this.Name = Name;
         this.pass = pass;
         this.email = email;
+        this.borrowedBooks = new ArrayList<>();
+        this.returnedBooks = new ArrayList<>();
     }
-    public users(){
+
+    public users() {
+        this.borrowedBooks = new ArrayList<>();
+        this.returnedBooks = new ArrayList<>();
     }
 
     public int getId() {
@@ -46,5 +55,35 @@ public class users {
 
     public void setName(String name) {
         Name = name;
+    }
+
+    public List<Books> getBorrowedBooks() {
+        return borrowedBooks;
+    }
+
+    public List<Books> getReturnedBooks() {
+        return returnedBooks;
+    }
+
+    public void borrowBook(Books book) {
+        if (book.isAvailable()) {
+            borrowedBooks.add(book);
+            book.setBorrowed(true);
+            book.setAvailableCopy(book.getAvailableCopy() - 1);
+            System.out.println("Book borrowed: " + book.getTitle());
+        } else {
+            System.out.println("Book is not available: " + book.getTitle());
+        }
+    }
+
+    public void returnBook(Books book) {
+        if (borrowedBooks.remove(book)) {
+            returnedBooks.add(book);
+            book.setBorrowed(false);
+            book.setAvailableCopy(book.getAvailableCopy() + 1);
+            System.out.println("Book returned: " + book.getTitle());
+        } else {
+            System.out.println("Book was not borrowed by this user: " + book.getTitle());
+        }
     }
 }
